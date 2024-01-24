8bf19f076495568e2ccdb228e232acb2bd778887
Christian Förster <christian.foerster@eawag.ch>
2024-01-24 16:36:21 +0100

    added upload via api decision tool

--- src/ckool/utilities.py
+++ src/ckool/utilities.py
@@ -20,6 +20,35 @@ def get_secret(name):
         return secret
 
 
+def upload_via_api(
+    files, space_available_on_server_root_disk, parallel_upload, factor: int = 4.8
+):
+    """
+    This function is used to decide, if data should be uploaded via the API or via the hacky way.
+    The hacky way requires uploading an empty resource followed by replacing the empty resource
+    with the actual file via SCP and updating the resource metadata.
+
+    During the upload via API and caching of multiple systems (uwsgi, nginx, python, ...)
+    each uploaded resource will quadruple in size (the factor parameter implements this behaviour),
+    especially for parallel uploads this should be considered to not fill up the root disk and crash the server
+    """
+
+    sizes = [file["size"] for file in files]
+    combined_size = sum(sizes)
+    max_single_size = max(sizes)
+
+    max_file_size = space_available_on_server_root_disk / factor
+
+    if any(
+        [
+            parallel_upload and combined_size < max_file_size,
+            not parallel_upload and max_single_size < max_file_size,
+        ]
+    ):
+        return True
+    return False
+
+
 def meta_default(
     pkg_name: str,
     filename: pathlib.Path,
