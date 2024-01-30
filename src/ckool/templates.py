d6bb9efd84ce40ffd0b6b0c791139904fd035ba5
Christian Förster <christian.foerster@eawag.ch>
2024-01-30 16:44:41 +0100

    template ideas

---
+++ src/ckool/templates.py
@@ -0,0 +1,40 @@
+def upload_data_package():
+    # datapackage_checker could also be included here
+
+    # local preparation - with file sizes and hashes
+
+    # create new package and upload all resources
+    # uploading data via scp or api (decided via file_size)
+
+    # check hashsums on remote system
+    pass
+
+
+def upload_resource():
+    # input can be folder -> that will be compressed, local preparation
+
+    # uploading to existing package
+
+    # check hashsums on remote system
+    pass
+
+
+def publish_package():
+    # download package check data consistency
+
+    # upload package to eric open
+
+    # publish to datacite
+
+    # update published package
+
+    pass
+
+
+def reserve_doi():
+    # package as input
+
+    # read metadata
+
+    # call datacite
+    pass
